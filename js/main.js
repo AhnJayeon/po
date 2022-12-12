2cc3979703ac6c2f89d9baf45b050b121e47a7b0
AhnJayeon <naturelike01@gmail.com>
2022-12-12 12:37:58 +0900

    메뉴모바일에 x토글 버튼 추가

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ btnCall.onclick = function(e){
     btnCall.classList.toggle("on");
     menuMo.classList.toggle('on');
 }
+//mobile 메뉴 내 close 버튼
+const close = document.querySelector(".close");
+close.onclick = function(e){
+  e.preventDefault();
+  close.classList.toggle("on");
+  menuMo.classList.toggle('on');
+}
 
 
 
