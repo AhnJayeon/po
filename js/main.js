ef78c25cd665958f5f82148c165ec756261cc6e0
AhnJayeon <naturelike01@gmail.com>
2022-12-12 14:33:17 +0900

    횡스크롤바 해결, but 많이 축소할 경우 스크롤바 이상 생김

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const menuMo = document.querySelector(".menuMo");
 btnCall.onclick = function(e){
     e.preventDefault();
     btnCall.classList.toggle("on");
-    menuMo.classList.toggle('on');
+    menuMo.classList.toggle("on");
 }
 //mobile 메뉴 내 close 버튼
 const close = document.querySelector(".close");
