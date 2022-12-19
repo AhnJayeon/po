84f9ec0a59f78970233ea97e7d35da7bd0a5ea39
AhnJayeon <naturelike01@gmail.com>
2022-12-19 16:24:11 +0900

    gallery4 (flickr페이지 작업 중)isotope문제있음

--- js/flickr.js
+++ js/flickr.js
@@ -9,36 +9,42 @@
 const base = "https://www.flickr.com/services/rest/?";
 const method = "flickr.interestingness.getList";
 const key = "0b209493cdb5598f801c755a108f8fa9";
-const per_page = 100;
+const per_page = 500;
 const format = "json";
-
-const frame = document.querySelector("#list");
 
 const url =`
 ${base}method=${method}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
 
+const frame = document.querySelector("#list");
+
+
 fetch(url)
-.then((data)=>{
+.then(data=>{
     let result = data.json();
     return result;
 })
-.then((json)=>{
+.then(json=>{
     let items = json.photos.photo;
-    console.log(items);
     
     let htmls = "";
-    items.map((data)=>{
+    items.map(data=>{
 
-        let imgSrc =`
-        <li class="item">
-        <div>
-        <a href=${imgSrcBig}>
-        <img src=${imgSrc}>
-        </a>
-        <p>${data.title}</p>
-        </div></li>
-        `;
+        let imgSrc =`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
+
+        let imgSrcBig = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
+
+        htmls += `
+                <li class="item">
+                    <div>
+                        <a href=${imgSrcBig}>
+                            <img src=${imgSrc}>
+                        </a>
+                        <p>${data.title}</p>
+                    </div>
+                </li>
+            `;
     })
+
     frame.innerHTML = htmls;
 
     const imgs = frame.querySelectorAll("img");
@@ -48,14 +54,12 @@ fetch(url)
     for(let el of imgs){
         el.onload = ()=>{
             count++;
-            if(count == len){
-                frame.classList.add("on");
-                Isotope();
+            if (count == len) isoLayout();
             }
         }
-    }
 })
-function Isotope() {
+function IsoLayout() {
+    frame.classList.add("on");
     new Isotope("#list", {
         itemSelector: ".item",
         columnWidth: ".item",
