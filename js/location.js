7820c8d9675de5bff78e3cdf6b5c55e4367c53fa
AhnJayeon <naturelike01@gmail.com>
2022-12-26 17:55:01 +0900

    profile페이지 작업중

--- js/location.js
+++ js/location.js
@@ -5,8 +5,8 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         center: new kakao.maps.LatLng(37.474736, 126.796695), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
-
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 
 
 function setCenter() {            
@@ -14,8 +14,8 @@ function setCenter() {
     var moveLatLon = new kakao.maps.LatLng(37.474736, 126.796695);
     
     // 지도 중심을 이동 시킵니다
-    map.setCenter(moveLatLon);
-}
+    map.setCenter(moveLatLon)
+};
 
 function panTo() {
     // 이동할 위도 경도 위치를 생성합니다 
@@ -23,8 +23,8 @@ function panTo() {
     
     // 지도 중심을 부드럽게 이동시킵니다
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-    map.panTo(moveLatLon);            
-}        
+    map.panTo(moveLatLon)          
+};        
 
 var markerPosition  = new kakao.maps.LatLng(37.474736, 126.796695); 
 
