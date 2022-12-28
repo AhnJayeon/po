4d9417d2ed0d97c5b07401fbc9ce5d294213aacb
AhnJayeon <naturelike01@gmail.com>
2022-12-28 12:49:58 +0900

    로그인창, 메인페이지 변경

--- js/main.js
+++ js/main.js
@@ -14,6 +14,13 @@ close.onclick = function(e){
   close.classList.toggle("on");
   menuMo.classList.toggle('on');
 }
+//로그인 창
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("on");
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("on");
+});
 
 //카카오 로그인
 window.Kakao.init("20657c743f4cbb1aaff6dbb2e6e714e3");
@@ -32,7 +39,12 @@ function KakaoLogin(){
         }
     })
 }
-
+// //LET'S 디자인
+// const lets = document.querySelector('.row h2');
+// const it = lets.innerText;
+// let tags = '';
+// for (const letter of it) tags += `<span>${letter}</span>`;
+// lets.innerHTML = tags;
 
 //gallery 문자열 바뀜
 const SHOWING_CLASS = "showing";
